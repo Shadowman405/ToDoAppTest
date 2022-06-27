8f3fe508ccda189c45aae3005f31a683af5e78ab
Maxim Mitin <maximus405@gmail.com>
2022-06-27 21:44:10 +0300

    add new Table VC  for categories add new Data Model link all with code

---
+++ ToDoAppTest/View/CategoryTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CategoryTableViewController.swift
+//  ToDoAppTest
+//
+//  Created by Maxim Mitin on 27.06.22.
+//
+
+import UIKit
+import CoreData
+
+class CategoryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+// MARK: - IBActions
+    
+    @IBAction func addButtonCLicked(_ sender: UIBarButtonItem) {
+    }
+    
+}
